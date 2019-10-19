e81c84179265228140f66d67d6b6449ac36564db
Saurabh Chaturvedi <>
2019-10-19 22:31:35 +0530

    [hithead] logic for finding prime numbers

---
+++ hithead/src/hithead/PrimeNumber.java
@@ -0,0 +1,21 @@
+package hithead;
+
+public class PrimeNumber {
+
+	public static boolean isPrime(int num) {
+		boolean isPrime = true;
+		for (int i = 2; i < num / 2; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return isPrime;
+
+	}
+
+	public static void main(String[] args) {
+		int x = 8;
+		boolean prime = isPrime(x);
+		System.out.println("number is prime " + prime);
+
+	}
+}
